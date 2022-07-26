97d765662554bef2100667077e7e54fa49fccd3f
Edoardo Soloperto <edoardo@poweringsrl.it>
2022-07-26 14:54:18 +0200

    add translation file

--- models/models.py
+++ models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, exceptions
+from odoo import models, fields, api, exceptions, _
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
@@ -11,6 +11,6 @@ class PurchaseOrder(models.Model):
     def button_confirm(self):
         for order in self:
             if order.delivery_date == False:
-                raise exceptions.Warning(('To confirm the order you have to indicate the delivery date.'))
+                raise exceptions.Warning(_("To confirm the order you have to indicate the delivery date."))
             else:
                 super(PurchaseOrder,self).button_confirm()
