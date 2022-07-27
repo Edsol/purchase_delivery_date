e7f74b522e654a3eb4a8f51914551193337597dd
Edoardo Soloperto <edoardo@poweringsrl.it>
2022-07-27 10:41:42 +0200

    add date check on save

--- models/models.py
+++ models/models.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, exceptions, _
+import logging
+_logger = logging.getLogger(__name__)
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
@@ -14,3 +16,16 @@ class PurchaseOrder(models.Model):
                 raise exceptions.UserError(_("To confirm the order you have to indicate the delivery date."))
             else:
                 super(PurchaseOrder,self).button_confirm()
+    
+    @api.multi
+    def write(self, vals):
+        if vals['delivery_date']:
+            date_order = fields.Date.from_string(self.date_order)
+            delivery_date = fields.Date.from_string(vals['delivery_date'])
+
+            if (delivery_date > date_order) == False:
+                raise exceptions.UserError(_("A delivery date prior to or equal to the order date cannot be entered."))
+            _logger.debug(delivery_date > date_order)
+        
+        res = super(PurchaseOrder, self).write(vals)
+        return res
